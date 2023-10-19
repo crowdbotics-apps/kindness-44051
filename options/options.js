79e424215ac018b0e831f0a364ab77fab5981c3c
Crowdbotics <team@crowdbotics.com>
2023-10-19 20:44:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kindness-44051",
+  url: "https://kindness-44051.botics.co",
+  api: "https://kindness-44051.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
